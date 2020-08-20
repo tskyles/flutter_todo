9de3f256a6d1db7d38bb2a80700bddaa22e7f576
travis skyles <travis.skyles@gmail.com>
2020-08-20 14:48:26 -0700

    create todo item class

---
+++ lib/model/todo.dart
@@ -0,0 +1,25 @@
+class Todo {
+  int id;
+  String description;
+  bool isDone = false;
+
+  Todo({this.id, this.description, this.isDone = false});
+
+  factory Todo.fromDatabaseJson(Map<String, dynamic> data) => Todo(
+        //This will be used to convert JSON objects that
+        //are coming from querying the database and converting
+        //it into a Todo object
+        id: data['id'],
+        description: data['description'],
+        //Since sqlite doesn't have boolean type for true/false
+        //we will 0 to denote that it is false
+        //and 1 for true
+        isDone: data['isDone'] == 0 ? false : true,
+      );
+
+  Map<String, dynamic> toDatabaseJson() => {
+        "id": this.id,
+        "description": this.description,
+        "isDone": this.isDone == false ? 0 : 1,
+      };
+}
